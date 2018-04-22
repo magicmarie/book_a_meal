66c8e23cdab85beef3f99b3c0397b1952375281c
magicmarie <natukunda162@gmail.com>
2018-04-22 18:07:07 +0300

    Initial Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: {
+        css: "./src/sass/app.scss",
+        js: "./src/js/app.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name]/app.[name]"
+    },
+    module: {
+        rules: [
+            {
+                test: path.resolve(__dirname, "src/sass/app.scss"),
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            }
+        ]
+    },
+    plugins: [new ExtractTextPlugin("css/app.css")]
+};
