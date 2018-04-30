3553bf6119bb732c06d485bf5e3eb2a16587f46d
magicmarie <natukunda162@gmail.com>
2018-04-30 16:28:09 +0300

    Restructure my app flow

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,21 @@
 # third-party imports
 import os
 from flask import Flask
+from flask_restful import Resource, Api
 from .config import app_config
 
 
 def create_app(config_name):
     # Initialize flask app
     app = Flask(__name__)
-    # load from config.py in root folder
-    app.config.from_object(app_config[config_name])
 
     return app
+
+
+app = create_app("testing")
+# load from config.py in root folder
+app.config.from_object(app_config["testing"])
+
+
+api = Api(app)
+from .api import views
