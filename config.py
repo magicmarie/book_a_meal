1267f50debfc36d0cee49a05ea36f2ddd504b64b
magicmarie <natukunda162@gmail.com>
2018-05-22 11:41:56 +0300

    [Feature #157764229] Boilerplate setup

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+
+class BaseConfig(object):
+    """
+    Common configurations
+    """
+    TESTING = False
+    DEBUG = False
+    SECRET_KEY = os.urandom(24)
+    # Put any configurations here that are common across all environments
+
+
+class TestingConfig(BaseConfig):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    DEBUG = True
+
+
+class DevelopmentConfig(BaseConfig):
+    """
+    Development configurations
+    """
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:magic@localhost/bookAmeal"
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
+
+if __name__ == '__main__':
+    app_config['development']
