4dc03984468b0fe74eb839f23d6f1ff1389d2e16
magicmarie <natukunda162@gmail.com>
2018-05-24 11:37:31 +0300

    [Feature #157768382] Write the signup endpoint to pass the test.

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from api import APP
 
 users = Blueprint('users', __name__)
 meals = Blueprint('meals', __name__)
@@ -6,3 +7,7 @@ menus = Blueprint('menus', __name__)
 orders = Blueprint('orders', __name__)
 
 from . import user_views, meal_views, menu_views, order_views
+APP.register_blueprint(users)
+APP.register_blueprint(meals)
+APP.register_blueprint(menus)
+APP.register_blueprint(orders)
