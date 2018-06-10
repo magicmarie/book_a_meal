5e78317f90632ac9bfcc772e81a45e062f797be4
magicmarie <natukunda162@gmail.com>
2018-06-10 19:02:28 +0300

    [Chore] Add API documentation

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 """ run server on port 5000"""
 from api import APP
+from flask import render_template
+
+
+@APP.route('/', methods=['GET'])
+def index():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     APP.run(debug=True)
