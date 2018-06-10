498c2ad37c4a45d389e3e6f7b4b9f0d5e3b0fc38
magicmarie <natukunda162@gmail.com>
2018-06-10 19:02:28 +0300

    [Chore] Add API documentation

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, reqparse, Resource
 from flask_migrate import Migrate
+from flasgger import Swagger
 
 
 # initialise app
@@ -24,6 +25,36 @@ migrate = Migrate(APP, DB)
 from .models import *
 with APP.app_context():
     DB.create_all()
+
+APP.config['swagger'] = {'swagger': '2.0', 'title': 'Book-a-meal-api', \
+            'description': "is a web based app that enables users to \
+            checkout menus, make orders and also check their order \
+            history. The meals, menus are made by the caterers, who \
+               view the user orders as well.",
+                         'basePath': '', 'version': '0.0.1', 'contact': {
+                             'Developer': 'Mariam Natukunda',
+                             'email': 'natukunda162@gmail.com'
+                         }, 'license': {
+                         }, 'tags': [
+                             {
+                                 'name': 'User',
+                                 'description': 'The api user'
+                             },
+                             {
+                                 'name': 'Meal',
+                                 'description': 'Meal options(create, read, \
+                                 update, delete) for a caterer'
+                             },
+                             {
+                                 'name': 'Menu',
+                                 'description': 'Menu a meal option is added to'
+                             },
+                             {
+                                 'name': 'Order',
+                                 'description': 'Meal request made by \
+                                  authenticated users'}]}
+
+swagger = Swagger(APP)
 
 from .views import *
 # register blueprints
